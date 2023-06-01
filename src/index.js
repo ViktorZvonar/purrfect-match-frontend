153b340a3a3bbf6d4ecf875288f87ba24f39a9c2
ViktorZvonar <viktorzvonar@yahoo.com>
2023-06-01 21:18:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="purrfect-match-frontend">
+        <BrowserRouter basename="/purrfect-match-frontend">
           <App />
         </BrowserRouter>
       </PersistGate>
